86931155b73272a13067750a7729416efe363ccb
govardhanreddyc79 <govardhanreddyc79@gmail.com>
2021-09-15 13:09:24 +0530

    logic done without css

---
+++ app.js
@@ -0,0 +1,43 @@
+const bill = document.querySelector("#bill-amount")
+const cash = document.querySelector("#cash-given")
+const btnCheck = document.querySelector("#btn-check")
+const message = document.querySelector("#error-message")
+const numberOfNotes = document.querySelectorAll(".no-of-notes")
+
+var availableNotes = [2000,500,100,20,10,5,1]
+
+function clickEventHandler(){
+    hideMessage();
+    if(bill.value > 0){
+        if(cash.value < bill.value){
+            showMessage("Do you wanna wash plates ?")
+        }else{
+            var amountLeft = cash.value-bill.value
+            calculateChange(amountLeft)
+        }
+
+    }else {
+        showMessage("Enter appropriate Bill Amount")
+    }
+    
+}
+ 
+function hideMessage(){
+    message.style.display = "none";
+}
+function showMessage(msg){
+    message.style.display = "block";
+    message.innerText = msg
+}
+
+function calculateChange(amount){
+    for(let i=0; i < availableNotes.length; i++){
+        var notes = Math.floor(amount / availableNotes[i])
+        console.log("notes :",notes)
+        amount = amount % availableNotes[i]
+        numberOfNotes[i].innerText = notes;
+    }
+
+}
+
+btnCheck.addEventListener("click",clickEventHandler)
