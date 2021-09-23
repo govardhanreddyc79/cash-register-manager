5e511bee1d82b6aaf0cfdf1ed21df4515d8b567c
govardhanreddyc79 <govardhanreddyc79@gmail.com>
2021-09-23 13:19:04 +0530

    modified return change table display

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const bill = document.querySelector("#bill-amount")
 const cash = document.querySelector("#cash-given")
 const btnCheck = document.querySelector("#btn-check")
-const message = document.querySelector("#error-message")
+const message = document.querySelector("#display-message")
 const numberOfNotes = document.querySelectorAll(".no-of-notes")
+var cashReturnTable = document.querySelector("#cash-return-table")
 
 var availableNotes = [2000,500,100,20,10,5,1]
+cashReturnTable.style.display = "none";
 
 btnCheck.addEventListener("click",function clickEventHandler(){
     hideMessage();
@@ -40,12 +42,17 @@ function showMessage(msg){
 }
 
 function calculateChange(amount){
-    for(let i=0; i < availableNotes.length; i++){
-        var notes = Math.floor(amount / availableNotes[i])
-        console.log("notes :",notes)
-        amount = amount % availableNotes[i]
-        numberOfNotes[i].innerText = notes;
-    }
+    if(amount>0){
+        cashReturnTable.style.display = "table";
+        for(let i=0; i < availableNotes.length; i++){
+            var notes = Math.floor(amount / availableNotes[i])
+            amount = amount % availableNotes[i]
+            numberOfNotes[i].innerText = notes;
+        }
+}else{
+    showMessage("Thank You !!!")
+
+}
 
 }
 
